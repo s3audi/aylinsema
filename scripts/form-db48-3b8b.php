6078d1b6c29b264e2f137c6eeb5adf285189f248
s3audi <s3audi@hotmail.com>
2025-02-26 17:55:54 +0300

    Aylin hanım revizyon talepleri KKB-BlogSac-Mezoterapi-Applications

---
+++ scripts/form-db48-3b8b.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'Yeni Form Gönder',
+    'email_message' => 'Yeni bir form gönderiminiz var',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 's3audi@hotmail.com',
+    'to' => 's3audi@hotmail.com',
+    'toCopy' => 's3audi@hotmail.com',
+    'toHiddenCopy' => 'semta@semta.com.tr'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Adınız Soyadınız',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Adınız Soyadınız\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Size nasıl yardımcı olabiliriz?',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Size nasıl yardımcı olabiliriz?\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
